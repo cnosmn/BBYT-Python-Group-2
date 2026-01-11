d57ce7d24d0deba45ee662ce7292a61a7423bfa3
cnosmn <cnosman14043@gmail.com>
2026-01-11 12:17:42 +0300

    fonksiyonlara giriş tekrar

---
+++ ders16.py
@@ -0,0 +1,46 @@
+# f(x) = ax + b
+# y = mx + n
+
+# collatz sanısı 
+
+# fonksiyon türleri
+# 1- parametresiz dönüşsüz
+def fonksiyon_ismi():
+    print("fonksiyon çalıştı")
+
+fonksiyon_ismi()
+
+def parametresiz_toplama():
+    a = 5
+    b = 35
+    print("a + b toplamı : ", a + b)
+
+parametresiz_toplama()
+
+def a():
+    print("zeynep sude oluşturabilyorkigibi")
+
+a()
+
+
+
+# 2- parametreli dönüşsüz
+# f(x) = ax + b
+# f(x1,x2) = ax1 + bx2 + c
+
+def parametreli_fonksiyon(sayi1,sayi2):
+    print("sayi1 + sayi2 toplamı : ", sayi1 + sayi2)
+
+def derin(kelime1,kelime2):
+    print("kelimelerin birleşmi : ",kelime1+kelime2)
+
+derin("merhaba","Zeynep")
+
+print("osman")
+print("can")
+print("selam")
+
+# 3- parametresiz dönüşlü
+# 4- parametreli dönüşlü
+
+
