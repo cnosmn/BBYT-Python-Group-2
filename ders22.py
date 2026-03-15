774d35b2e7b13c8b8a8ba8eebcd4829ae4439a71
cnosmn <cnosman14043@gmail.com>
2026-03-15 08:53:04 +0300

    Create ders22.py

---
+++ ders22.py
@@ -0,0 +1,37 @@
+
+
+# Hastane Otomasyonu
+
+# randevu
+# hasta
+# doktor
+# polikinlik
+# bölüm
+# tahlil
+# hastanePersoneli
+# ilaç
+
+"""
+RANDEVU SİSTEMİ
+
+class Insan
+isim
+soyisim
+cinsiyet
+yaş
+
+class Doktor : Insan
+alan
+ünvan
+musaitlik -> {"pazartesi":[9,10,11,12,14,15,16,17]}
+
+class Hasta : Insan
+randevu_listesi
+alerjiler
+tc_no
+boy
+kilo
+
+"""
+
+
