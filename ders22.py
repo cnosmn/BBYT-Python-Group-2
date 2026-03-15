4cf998ebfb52aa54b36ddce385bb0424042f6ca2
cnosmn <cnosman14043@gmail.com>
2026-03-15 18:01:40 +0300

    Update ders22.py

--- ders22.py
+++ ders22.py
@@ -34,4 +34,41 @@ kilo
 
 """
 
+# https://codeshare.io/aVnxq9
+
+class Insan:
+    def __init__(self,
+    isim,
+    soyisim,
+    cinsiyet,
+    yas    ):
+        self.__isim = isim
+        self.__soyisim = soyisim
+        self.__cinsiyet = cinsiyet
+        self.__yas = yas
+    
+    def bilgileri_goster(self):
+        print("isim: ", self.__isim)
+        print("soyisim: ", self.__soyisim)
+        print("cinsiyet: ", self.__cinsiyet)
+        print("yas: ", self.__yas)
+        
+insan1 = Insan("osman","can","erkek",10)
+
+class Doktor(Insan):
+    def __init__(self, isim,soyisim, cinsiyet, yas,alan,unvan,musaitlik):
+        super().__init__(isim,soyisim,cinsiyet,yas)
+        self.__alan = alan
+        self.__unvan = unvan
+        self.__musaitlik = musaitlik
+
+class Hasta(Insan):
+    def __init__(self, isim,soyisim,cinsiyet, yas,randevu_listesi,alerjiler,tc_no):
+        super().__init__(isim,soyisim,cinsiyet,yas)
+        self.__randevu_listesi = randevu_listesi
+        self.__alerjiler = alerjiler
+        self.__tc_no = tc_no
+
+    
+
 
